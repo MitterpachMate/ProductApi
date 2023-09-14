4275e605cfe9be06b3b11882c45be6197e4b61de
mmáté <mitterpachm@kkszki.hu>
2023-09-14 08:36:16 +0200

    public record letrehozva a dtos.cs-ben

--- ProductApi/ProductApi/dtos.cs
+++ ProductApi/ProductApi/dtos.cs
@@ -1,6 +1,9 @@
-﻿namespace ProductApi
+﻿using Microsoft.VisualBasic;
+
+namespace ProductApi
 {
     public class dtos
     {
+        public record productdto(Guid id, string product_neve, int ar, DateTimeOffset keszitett_ido, DateTimeOffset modositas_ido);
     }
 }
