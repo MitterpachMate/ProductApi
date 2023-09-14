52b9a52bcd0657fba3e9d583abc7474a46ece233
mmáté <mitterpachm@kkszki.hu>
2023-09-14 08:44:59 +0200

    lancolt lista letrehozas, termek hozzaadas

--- ProductApi/ProductApi/Controllers/ProductController.cs
+++ ProductApi/ProductApi/Controllers/ProductController.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using static ProductApi.dtos;
 
 namespace ProductApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class ProductController : ControllerBase
     {
+        private static readonly List<productdto> product = new()
+        {
+        new productdto(Guid.NewGuid(), "Termék1", 2500, DateTimeOffset.UtcNow, DateTimeOffset.UtcNow),
+        new productdto(Guid.NewGuid(), "Termék2", 500, DateTimeOffset.UtcNow, DateTimeOffset.UtcNow),
+        new productdto(Guid.NewGuid(), "Termék3", 3500, DateTimeOffset.UtcNow, DateTimeOffset.UtcNow),
+        new productdto(Guid.NewGuid(), "Termék4", 10000, DateTimeOffset.UtcNow, DateTimeOffset.UtcNow),
+        };
     }
 }
